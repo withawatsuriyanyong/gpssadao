50bf233088d179494c1e08d4ccee8a7a35dce30d
withawatsuriyanyong <armery@gmail.com>
2024-06-04 15:37:21 +0700

    first commit

---
+++ GPSSadao01/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GPSSadao01.Startup))]
+namespace GPSSadao01
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
